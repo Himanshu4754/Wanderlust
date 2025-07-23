f7a153e66bc31e9ecd4537fbcb4ec8ab8f68e986
Himanshu4754 <himanshugurjar1306@gmail.com>
2025-07-23 21:19:51 +0530

    first commit

---
+++ models/listing.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; 
+
+const listingSchema = new Schema ({
+    title : {
+        type : String,
+        required : true,
+    },
+    description : String,
+    image : {
+        filename : String,
+        url : {
+        type : String,
+        default : "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
+        set: (v) => v === "" ? "https://images.unsplash.com/photo-1506744038136-46273834b3fb" : v,
+        }
+    },
+    price : Number,
+    location : String,
+    country : String, 
+});
+
+const Listing = mongoose.model("Listing",listingSchema);
+module.exports = Listing;

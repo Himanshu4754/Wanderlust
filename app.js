d26ffdf1ca38411ce7026c5ddaaf56b15e6342c4
Himanshu4754 <himanshugurjar1306@gmail.com>
2025-08-20 00:40:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.all("/:var",(req,res,next) => {
 
 app.use((err,req,res,next) => {
     let {statusCode = 500, message = "Something went wrong!"} = err;
-    res.status(statusCode).render("error.ejs",{ message });
+    res.status(statusCode).render("error.ejs",{ message });""
 });
 
 

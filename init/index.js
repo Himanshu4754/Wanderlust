90321e01877de2eac36d5800d16c744c058d2c12
Himanshu4754 <himanshugurjar1306@gmail.com>
2025-08-20 00:38:35 +0530

    Add Project Files

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "6892154a2894c22139123422" }));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }

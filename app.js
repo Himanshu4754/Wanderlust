934650f7f1c42b6ea2043bc7198adc5b37468a3d
Himanshu4754 <himanshugurjar1306@gmail.com>
2025-08-26 12:21:47 +0530

    Fix: added Listing model import and resolved conflicts

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+const Listing = require("./models/listing.js");
+
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");

00b2a4e77c80d9861d34f96c5b5323925f805ac3
Himanshu4754 <himanshugurjar1306@gmail.com>
2025-12-24 16:07:02 +0530

    css edited

--- public/css/js/map.js
+++ public/css/js/map.js
@@ -1,45 +1,22 @@
-if (typeof listing !== 'undefined' && listing.geometry && listing.geometry.coordinates) {
-    mapboxgl.accessToken = mapToken;
+mapboxgl.accessToken = mapToken;
 
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: "mapbox://styles/mapbox/streets-v12",
-        center: listing.geometry.coordinates,
-        zoom: 9
-    });
-if (typeof listing !== 'undefined' && listing.geometry && listing.geometry.coordinates) {
-    mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: "mapbox://styles/mapbox/streets-v12", // style url
+    center: listing.geometry.coordinates, // starting position [lng, lat]
+    zoom: 9  // starting zoom
+});
 
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: "mapbox://styles/mapbox/streets-v12",
-        center: listing.geometry.coordinates,
-        zoom: 9
-    });
-
-    new mapboxgl.Marker({ color: "red" })
-        .setLngLat(listing.geometry.coordinates)
+ new mapboxgl.Marker({ color: "red" })
+        .setLngLat(listing.geometry.coordinates) //listing.geometry.coordinates
         .setPopup(
             new mapboxgl.Popup({offset: 25}).setHTML(
                 `<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`
+                )
             )
-        )
         .addTo(map);
 
-    console.log("Map initialized with coordinates:", listing.geometry.coordinates);
-} else {
-    console.error("Listing geometry data not available");
-}
-    new mapboxgl.Marker({ color: "red" })
-        .setLngLat(listing.geometry.coordinates)
-        .setPopup(
-            new mapboxgl.Popup({offset: 25}).setHTML(
-                `<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`
-            )
-        )
-        .addTo(map);
+        console.log("Listing geometry:", listing.geometry);
 
-    console.log("Map initialized with coordinates:", listing.geometry.coordinates);
-} else {
-    console.error("Listing geometry data not available");
-}
+
+  
